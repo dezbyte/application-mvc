014950e2f730f374243da3d34c1b68e3fa63f490
Ivan Gontarenko <g.ivan@dizboard.com>
2015-10-28 14:48:51 +0200

    services

--- src/Mvc/Application.php
+++ src/Mvc/Application.php
@@ -39,6 +39,8 @@
 
     class Application extends Injectable {
 
+        protected $page404Handler;
+
         public function __construct()
         {
             $this->setDi( FactoryContainer::instance() );
@@ -58,28 +60,55 @@
 
             $router->handle();
 
-            if( ! $router->isFounded() ) {
+            if( $router->isFounded() ) {
+
+                $dispatcher = new ControllerDispatcher( $this->getDi() );
+
+                $dispatcher->setName( $router->getController() );
+                $dispatcher->setAction( $router->getAction() );
+                $dispatcher->setParams( $router->getMatches() );
+
+                $dispatcher->dispatch();
+
+                $this->view->addLayout( "layouts/{$router->getController()}" );
+                $content    = $this->view->render( "{$router->getController()}/{$router->getAction()}" );
+
+                $this->response->setContent( $content );
+
+                $this->event->dispatch( 'afterApplicationRun', new MvcEvent( $this ) );
+
+
+            } else {
                 $this->event->dispatch( 'onPageNotFound', new MvcEvent( $this ) );
-                throw new ControllerException( 'Route not found' );
+                $this->response->setStatusCode( 404 );
+
+                if( $this->getPage404Handler() instanceof \Closure ) {
+                    call_user_func_array( $this->getPage404Handler(), [ $this ] );
+                    $this->response->setContent( $this->view->render( 'error404.php' ) );
+                } else {
+                    throw new ControllerException( "Page {$this->request->getServer( 'request_uri' )} not found" );
+                }
             }
 
-            $dispatcher = new ControllerDispatcher( $this->getDi() );
-
-            $dispatcher->setName( $router->getController() );
-            $dispatcher->setAction( $router->getAction() );
-            $dispatcher->setParams( $router->getMatches() );
-
-            $dispatcher->dispatch();
-
-            $this->view->addLayout( "layouts/{$router->getController()}" );
-            $content    = $this->view->render( "{$router->getController()}/{$router->getAction()}" );
-
-            $this->response->setContent( $content );
-
-            $this->event->dispatch( 'afterApplicationRun', new MvcEvent( $this ) );
             return $this->response;
         }
 
+        /**
+         * @return \Closure
+         */
+        public function getPage404Handler()
+        {
+            return $this->page404Handler;
+        }
 
+        /**
+         * @param \Closure $page404Handler
+         * @return $this
+         */
+        public function setPage404Handler( \Closure $page404Handler )
+        {
+            $this->page404Handler = \Closure::bind( $page404Handler, $this );
+            return $this;
+        }
 
     }
